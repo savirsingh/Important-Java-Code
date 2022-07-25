68962d304c57f7b7ed590d05f20460e6cdcb9244
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-25 19:32:28 -0400

    Create Inputs.java

---
+++ Inputs & Conversion/Inputs.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Inputs {
+    public static void main(String[] args) throws Exception {
+      
+      Scanner br = new Scanner(System.in); 
+      
+      String getbr = br.nextLine();
+    }
+}
