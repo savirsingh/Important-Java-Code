cb8d8b12289d0769da1b56cc68c68680e7a277c3
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-25 19:28:53 -0400

    Create ConvertStrToInt.java

---
+++ Inputs & Conversion/ConvertStrToInt.java
@@ -0,0 +1,13 @@
+// code by savir singh
+
+import java.util.*;
+
+public class ConvertStrToInt {
+    public static void main(String[] args) throws Exception {
+      
+      Scanner br = new Scanner(System.in); 
+      System.out.println("Enter number"); 
+      int myInt = br.nextInt();
+      System.out.println(myInt);
+    }
+}
