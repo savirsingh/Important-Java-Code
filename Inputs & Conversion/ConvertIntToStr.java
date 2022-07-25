6cbb4867204a940d7b8160a993efcff4ab3b5e3d
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-25 19:31:30 -0400

    Create ConvertIntToStr.java

---
+++ Inputs & Conversion/ConvertIntToStr.java
@@ -0,0 +1,11 @@
+// code by savir singh
+
+public class ConvertIntToStr {
+    public static void main(String[] args) throws Exception {
+      
+      int a = 8;
+      String converted = String.valueOf(a);
+     
+      System.out.println(converted);
+    }
+}
